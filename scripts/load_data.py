08c8f01429daade32f148e1ab383bfaf29f8caf2
ya_sonia <soniayadavnitsgr@gmail.com>
2026-05-14 16:51:00 +0530

    data path added

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,11 +1,6 @@
 import pandas as pd
 from db_connection import engine
 
-# Load cleaned dataset
-# df = pd.read_csv(
-#     r"C:\Users\hp\Documents\Projects_BTech\Business Analyst Project\Datas\clean_student_dropout.csv"
-# )
-
 df = pd.read_csv("Datas/clean_student_dropout.csv")
 df.head()
 
